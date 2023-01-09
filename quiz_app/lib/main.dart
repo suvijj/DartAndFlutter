edf75a238d36ddc71ecb6eab42b949aeaffefe4f
suvijj <suvi.jylha@outlook.com>
2023-01-09 16:06:36 +0200

    kysymyssovelluksen tekoa ohjeiden mukaan

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() {
- runApp(QuizApp());
+  runApp(MyApp());
 }
 
-class QuizApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+var questionIndex = 0;
+
+void answerQuestion() {
+  setState(() {
+      questionIndex = questionIndex + 1;
+  });
+
+  print('Answer chosen');
+}
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'Do you like hedgehogs?',
+      'Do you eat meat?',
+    ];
     return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('Quiz App'),
+      appBar: AppBar(title: Text('Quiz App'),), //Appbar
+      body: Column(
+        children: [
+          Question(
+            questions[questionIndex]
+          ),
+          ElevatedButton(child: Text('Yes'), onPressed: answerQuestion,),
+          ElevatedButton(child: Text('A bit'), onPressed: answerQuestion,),
+          ElevatedButton(child: Text('No'), onPressed: answerQuestion,),
+        ]),
       ),
-      body: Text('Default'),
-    ),);
+      );
   }
 }
